41b771d9515c3b57cdc9062dc911a66c75d84e3c
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-27 01:17:47 +0530

    Ability to create BST by adding 56 and 30 & 70

--- src/com/binarysearchtree/MyBinaryTest.java
+++ src/com/binarysearchtree/MyBinaryTest.java
@@ -1,8 +1,31 @@
 package com.binarysearchtree;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyBinaryTest {
 
-	
-
+	@Test
+	public void given3NumbersWhenAddedToBinaryShouldReturnSizeThree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		//left subtree
+		{myBinaryTree.add(30);
+		{myBinaryTree.add(22);
+		{myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);}}
+		myBinaryTree.add(40);}
+		//Right subtree
+		{myBinaryTree.add(70);
+		{myBinaryTree.add(60);
+		{myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);}}
+		myBinaryTree.add(95);}
+		
+		int size = myBinaryTree.getSize();
+		Assert.assertTrue(myBinaryTree.search(40));
+	}
 }
--- src/com/binarysearchtree/MyBinaryTree.java
+++ src/com/binarysearchtree/MyBinaryTree.java
@@ -1,5 +1,46 @@
 package com.binarysearchtree;
 
-public class MyBinaryTree {
+public class MyBinaryTree<K extends Comparable<K>> {
+	private MyBinaryNode<K> root;
 
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
+	}
+
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+		if (current == null)
+			return new MyBinaryNode<>(key);
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0)
+			return current;
+		if (compareResult < 0)
+			current.left = addRecursively(current.left, key);
+		else
+			current.right = addRecursively(current.right, key);
+		return current;
+	}
+
+	int getSize() {
+		return this.getSizeRecursive(root);
+	}
+
+	private int getSizeRecursive(MyBinaryNode<K> current) {
+		return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+	}
+	
+	public boolean search(K key) {
+		return this.searchRecursively(root, key);
+	}
+
+	private boolean searchRecursively(MyBinaryNode<K> current, K key) {
+		if (current == null)
+			return false;
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0)
+			return true;
+		if (compareResult < 0)
+			return searchRecursively(current.left, key);
+		else
+			return searchRecursively(current.right, key);
+	}
 }
